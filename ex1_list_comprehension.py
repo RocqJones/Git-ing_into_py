001ea4ae2542b2ec979fb8fd80c42b97dc2963ef
RocqJones <rocqjones@gmail.com>
2018-09-21 14:53:45 +0300

    ex1

---
+++ ex1_list_comprehension.py
@@ -0,0 +1,9 @@
+#list comprehension vs looping
+numbers = [1,2,3,4,5]
+doubled_numbers = []
+
+for num in numbers:
+    doubled_number = num * 2
+    doubled_numbers.append(doubled_number)
+    
+print(doubled_numbers)
